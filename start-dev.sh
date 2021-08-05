9187fcbf0dbe072f222e019c8a01881c9dee7890
svillechenau <simon.villechenaud@etu.u-bordeaux.fr>
2021-08-05 19:08:44 +0200

    Start 1 more git bash

--- start-dev.sh
+++ start-dev.sh
@@ -3,4 +3,5 @@
 chrome.exe https://les-randoms.herokuapp.com/ https://github.com/Vemuni/les-randoms https://phpmyadmin.alwaysdata.com/phpmyadmin/index.php?route=/&route=%2F&lang=en http://localhost:5000/ &
 code . &
 git-bash.exe &
+git-bash.exe &
 cmd
