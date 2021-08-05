14ac2ff3f0c4abbda26ba578e8da4bafbad6d862
svillechenau <simon.villechenaud@etu.u-bordeaux.fr>
2021-08-05 23:19:58 +0200

    Fix Launching Bug

--- database/database.go
+++ database/database.go
@@ -2,7 +2,6 @@ package database
 
 import (
 	"database/sql"
-	"fmt"
 	"les-randoms/utils"
 	"os"
 
@@ -12,16 +11,9 @@ import (
 var Database *sql.DB
 
 func OpenDatabase() {
-	Database, err := sql.Open("mysql", os.Getenv("DATABASE_CONNECTION_STRING"))
+	var err error
+	Database, err = sql.Open("mysql", os.Getenv("DATABASE_CONNECTION_STRING"))
 	utils.HandlePanicError(err)
-
-	var testValue string
-	err = Database.QueryRow("SELECT name FROM BlackListItem WHERE id=1").Scan(&testValue)
-	if err != nil {
-		panic(err.Error())
-	}
-
-	fmt.Println("DATABASE.TEST() - END - VAR:" + testValue)
 }
 
 func SelectDatabase(queryBody string) (*sql.Rows, error) {
--- start-dev.sh
+++ start-dev.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-chrome.exe https://les-randoms.herokuapp.com/ https://github.com/Vemuni/les-randoms https://phpmyadmin.alwaysdata.com/phpmyadmin/index.php?route=/&route=%2F&lang=en http://localhost:5000/ &
+chrome.exe https://les-randoms.herokuapp.com/ https://github.com/Vemuni/les-randoms https://admin.alwaysdata.com/database/?type=mysql http://localhost:5000/ &
 code . &
 git-bash.exe &
 git-bash.exe &
