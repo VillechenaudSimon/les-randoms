b09c49cb7f2ff42c13cd5a2f1c40cd2d7737c7ae
svillechenau <simon.villechenaud@etu.u-bordeaux.fr>
2021-08-31 15:58:40 +0200

    Set GoldenList as default choice in /aram

--- webserver/aram.go
+++ webserver/aram.go
@@ -26,29 +26,32 @@ func handleAramRoute(c *gin.Context) {
 	data.LayoutData.SubnavData.SubnavItems = append(data.LayoutData.SubnavData.SubnavItems, subnavItem{Name: "Bot List"})
 	//data.LayoutData.SubnavData.SubnavItems = append(data.LayoutData.SubnavData.SubnavItems, subnavItem{Name: "Tier List"})
 	data.LayoutData.SubnavData.SelectedSubnavItemIndex = 0
+
+	selectedItemName := "Golden List"
 	if c.Request.Method == "POST" {
-		selectedItemName := c.PostForm("subnavSelectedItem")
-		for i := 0; i < len(data.LayoutData.SubnavData.SubnavItems); i++ {
-			if selectedItemName == data.LayoutData.SubnavData.SubnavItems[i].Name {
-				data.LayoutData.SubnavData.SelectedSubnavItemIndex = i
-				break
-			}
-		}
+		selectedItemName = c.PostForm("subnavSelectedItem")
+	}
 
-		data.ListTableData = customTableData{}
-		list, err := database.List_SelectFirst("WHERE name = " + utils.Esc(selectedItemName))
-		if err != nil {
-			redirectToIndex(c)
-			utils.LogError("Error while creating customTableData item with a DB List (Selected list : " + selectedItemName + ") : " + err.Error())
-			return
+	for i := 0; i < len(data.LayoutData.SubnavData.SubnavItems); i++ {
+		if selectedItemName == data.LayoutData.SubnavData.SubnavItems[i].Name {
+			data.LayoutData.SubnavData.SelectedSubnavItemIndex = i
+			break
 		}
-		data.ListTableData.HeaderList = list.Headers
+	}
 
-		listItems, err := database.ListItem_SelectAll("WHERE listId = " + fmt.Sprint(list.Id) + " ORDER BY date")
-		data.ListTableData.ItemList = make([]tableItemData, 0)
-		for _, listItem := range listItems {
-			data.ListTableData.ItemList = append(data.ListTableData.ItemList, tableItemData{FieldList: append([]string{listItem.Date.Local().Format(utils.DateFormat)}, utils.ParseDatabaseStringList(listItem.Value)...)})
-		}
+	data.ListTableData = customTableData{}
+	list, err := database.List_SelectFirst("WHERE name = " + utils.Esc(selectedItemName))
+	if err != nil {
+		redirectToIndex(c)
+		utils.LogError("Error while creating customTableData item with a DB List (Selected list : " + selectedItemName + ") : " + err.Error())
+		return
+	}
+	data.ListTableData.HeaderList = list.Headers
+
+	listItems, err := database.ListItem_SelectAll("WHERE listId = " + fmt.Sprint(list.Id) + " ORDER BY date")
+	data.ListTableData.ItemList = make([]tableItemData, 0)
+	for _, listItem := range listItems {
+		data.ListTableData.ItemList = append(data.ListTableData.ItemList, tableItemData{FieldList: append([]string{listItem.Date.Local().Format(utils.DateFormat)}, utils.ParseDatabaseStringList(listItem.Value)...)})
 	}
 
 	c.HTML(http.StatusOK, "aram.tmpl.html", data)
