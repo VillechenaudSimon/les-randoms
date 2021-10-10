b79217f942c85fb43849b090335ff8051b2bfb96
Simon Villechenaud <61287756+Vemuni@users.noreply.github.com>
2021-10-10 16:30:21 +0200

    Update local-run.sh

--- local-run.sh
+++ local-run.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 # Fill the next lines with the database connection string, the discord app clientId, the discord app clientSecret and the website url
-export DATABASE_CONNECTION_STRING="217240:Rj4s7E!s*hBf@tcp(mysql-villechenaud-simon.alwaysdata.net:3306)/villechenaud-simon_les-randoms"
-export DISCORD_CLIENTID="481156786779324416"
-export DISCORD_CLIENTSECRET="Nlzu8wj6z_yb-IZIjeAzQZPZmsVEG8Hu"
-export WEBSITE_URL="http://localhost:5000"
+export DATABASE_CONNECTION_STRING=""
+export DISCORD_CLIENTID=""
+export DISCORD_CLIENTSECRET=""
+export WEBSITE_URL=""
 #
 
 echo "Ctrl+C to stop running the local server"
