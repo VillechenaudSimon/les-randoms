281df1026dd006433f0ab7cd07ac5e734bfab682
svillechenau <simon.villechenaud@etu.u-bordeaux.fr>
2021-11-21 20:27:46 +0100

    Remove one git-bash instance on start-dev

--- start-dev.sh
+++ start-dev.sh
@@ -3,5 +3,4 @@
 chrome.exe https://les-randoms.herokuapp.com/ https://github.com/Vemuni/les-randoms https://admin.alwaysdata.com/database/?type=mysql http://localhost:5000/ &
 code . &
 git-bash.exe &
-git-bash.exe &
 cmd
