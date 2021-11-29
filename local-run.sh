f3bb6c6610c90471111fc045a27d600b923c6b9b
svillechenau <simon.villechenaud@etu.u-bordeaux.fr>
2021-11-30 00:02:42 +0100

    Don't start local server if build failed

--- local-run.sh
+++ local-run.sh
@@ -7,5 +7,4 @@ export WEBSITE_URL="http://localhost:5000"
 
 echo "Ctrl+C to stop running the local server"
 echo "See on $WEBSITE_URL"
-go build -o bin/les-randoms.exe -v .
-heroku local web -f Procfile.windows
+go build -o bin/les-randoms.exe -v . && heroku local web -f Procfile.windows
