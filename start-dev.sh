421e25090ef2e05c35f4b00da1260fb51dd52922
svillechenau <simon.villechenaud@etu.u-bordeaux.fr>
2021-12-05 00:54:04 +0100

    Edit start-dev script

--- start-dev.sh
+++ start-dev.sh
@@ -2,5 +2,9 @@
 
 chrome.exe https://les-randoms.herokuapp.com/ https://github.com/Vemuni/les-randoms https://admin.alwaysdata.com/database/?type=mysql http://localhost:5000/ &
 code . &
+
+# Use this git bash to git add/commit/push and to ./local-run.sh
 git-bash.exe &
-cmd
+
+# Use this git bash to heroku login and git push heroku main and git logs --tail
+git-bash.exe &
