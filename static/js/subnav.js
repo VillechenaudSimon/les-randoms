37acc0fe7a7f1f0e9de04761a0ea2e95ccc2c6eb
svillechenau <simon.villechenaud@etu.u-bordeaux.fr>
2021-12-05 18:59:38 +0100

    Fix 3+ words subnav item url

--- static/js/subnav.js
+++ static/js/subnav.js
@@ -2,6 +2,6 @@ $(document).ready(function () {
     $(".subnav").find("button.confirm").click(function (e) {
         e.preventDefault()
         urlParts = window.location.href.split('/')
-        window.location.href = urlParts[0] + '/' + urlParts[1] + '/'  + urlParts[2] + '/'  + urlParts[3] + '/' + $(this)[0].innerHTML.replace(" ", '')
+        window.location.href = urlParts[0] + '/' + urlParts[1] + '/'  + urlParts[2] + '/'  + urlParts[3] + '/' + $(this)[0].innerHTML.replaceAll(" ", '')
     });
 });
