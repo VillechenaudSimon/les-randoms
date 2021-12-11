247bb50dba7cc171c686ab9ffd2df41ad67b232a
svillechenau <simon.villechenaud@etu.u-bordeaux.fr>
2021-12-11 13:12:38 +0100

    Add buttons that sort custom tables

--- static/js/customTable.js
+++ static/js/customTable.js
@@ -1,16 +1,34 @@
 $(document).ready(function () {
-    tableToSort = $(".customTable.sorted")
-    sortTable(tableToSort, parseInt(tableToSort[0].style.getPropertyValue('--sortColumnIndex')), parseInt(tableToSort[0].style.getPropertyValue('--sortOrder'))) // Uncomment this last parameter to sort in ascending order
+    // Make initial sorting
+    sortTable($(".customTable.sorted"))
+
+    // Add buttons on the table header to sort it as wanted
+    $(".customTable thead tr th").click(function (e) {
+        e.preventDefault();
+        newSortColumnIndex = $(this).index()
+        table = $(this).parents(".customTable")[0]
+        if (parseInt(table.style.getPropertyValue('--sortColumnIndex')) == newSortColumnIndex) { // This part needs to be optimised (don't sort just reverse elements order)
+            if (table.style.getPropertyValue('--sortOrder') == 0) {
+                table.style.setProperty('--sortOrder', 1)
+            } else {
+                table.style.setProperty('--sortOrder', 0)
+            }
+        } else {
+            table.style.setProperty('--sortColumnIndex', newSortColumnIndex)
+            table.style.setProperty('--sortOrder', 0)
+        }
+        sortTable($(this).parents(".customTable"))
+    });
 });
 
-function sortTable(table, columnIndexSort, sortOrder) {
+function sortTable(table) {
     tbody = table.find("tbody")[0]
     if (tbody != undefined) {
         trList = tbody.getElementsByTagName("tr")
         /*for (i = 0; i < trList.length; i++) {
             console.log(trList[i].children[columnIndexSort].innerHTML + " - " + trList[i].children[columnIndexSort + 1].innerHTML)
         }*/
-        quickSortTrList(trList, 0, tbody.children.length-1, columnIndexSort, sortOrder)
+        quickSortTrList(trList, 0, tbody.children.length - 1, parseInt(table[0].style.getPropertyValue('--sortColumnIndex')), parseInt(table[0].style.getPropertyValue('--sortOrder')))
     }
 }
 
@@ -23,12 +41,14 @@ function quickSortTrList(trList, iStart, iEnd, columnIndexSort, sortOrder) {
 }
 
 function quickSortTrListPartition(trList, iStart, iEnd, columnIndexSort, sortOrder) {
-    pivot = parseInt(trList[iEnd].children[columnIndexSort].innerHTML)
+    pivot = parseIfNeeded(trList[iEnd].children[columnIndexSort].innerHTML)
+
     //console.log(pivot + " - " + trList[iEnd].children[1].innerHTML)
     i = iStart - 1
 
     for (j = iStart; j <= iEnd - 1; j++) {
-        if (quickSortVerifyOrder(parseInt(trList[j].children[columnIndexSort].innerHTML), pivot, sortOrder)) {
+        value = parseIfNeeded(trList[j].children[columnIndexSort].innerHTML)
+        if (quickSortVerifyOrder(value, pivot, sortOrder)) {
             i++
             swap(trList, i, j)
         }
@@ -37,13 +57,20 @@ function quickSortTrListPartition(trList, iStart, iEnd, columnIndexSort, sortOrd
     return i + 1
 }
 
+function parseIfNeeded(value) {
+    if (isNaN(value) || value > 2000) { // Ugly fix but xd
+        return value.replace(/ /g, '')
+    }
+    return parseInt(value)
+}
+
 // order == 1 means <
 // else means >
-function quickSortVerifyOrder(value1, value2, order) {
+function quickSortVerifyOrder(value, pivot, order) {
     if (order == 1) {
-        return value1 < value2
+        return value < pivot
     } else {
-        return value1 > value2
+        return value > pivot
     }
 }
 
