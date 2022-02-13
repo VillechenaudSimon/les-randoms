38fff72b1d617e8173563df164a7e1c63455554e
svillechenau <simon.villechenaud@etu.u-bordeaux.fr>
2022-02-13 19:36:15 +0100

    Optimize backgroundworker ticking

--- backgroundworker/backgroundworker.go
+++ backgroundworker/backgroundworker.go
@@ -1,17 +1,18 @@
 package backgroundworker
 
 import (
-	"fmt"
 	"les-randoms/utils"
 	"time"
 )
 
 // tickerUpdateSpacing > tickerTickSpacing Or there will be problems
-//const tickerTickSpacing time.Duration = time.Minute
-//const tickerUpdateSpacing time.Duration = 10 * tickerTickSpacing
 
 const tickerTickSpacing time.Duration = 2 * time.Second
+
+//const tickerTickSpacing time.Duration = time.Minute
 const tickerUpdateSpacing time.Duration = 10 * tickerTickSpacing
+
+var currentTickerTickSpacing = tickerTickSpacing * 9
 
 var JobAdder chan Job = make(chan Job)
 var lastUpdateTime time.Time
@@ -38,7 +39,7 @@ func Start() {
 		case job := <-JobAdder:
 			jobs = append(jobs, &job)
 		case job := <-jobQueue:
-			utils.LogDebug("DOING JOB ID : " + fmt.Sprint(job.Id))
+			//utils.LogDebug("DOING JOB ID : " + fmt.Sprint(job.Id))
 			job.Do(&job.Memory)
 			job.LastDoneTime = time.Now().UTC()
 		}
@@ -47,13 +48,15 @@ func Start() {
 
 func startTicker(c chan *Job) {
 	for {
-		utils.LogDebug("Ticking.. " + lastUpdateTime.Format(utils.DateTimeFormat) + " - " + fmt.Sprint(len(jobs)) + " jobs")
-		time.Sleep(tickerTickSpacing)
+		time.Sleep(currentTickerTickSpacing)
+		currentTickerTickSpacing = tickerTickSpacing / 3
+		//utils.LogDebug("Ticking.. " + lastUpdateTime.Format(utils.DateTimeFormat) + " - " + fmt.Sprint(len(jobs)) + " jobs")
 		if time.Now().UTC().Sub(lastUpdateTime) > tickerUpdateSpacing {
-			//utils.LogDebug("Updating.. " + lastUpdateTime.Format(utils.DateTimeFormat) + " - " + fmt.Sprint(len(jobs)) + " jobs" + " - J0:" + fmt.Sprint(jobs[0].Id) + " - J1:" + fmt.Sprint(jobs[1].Id))
+			currentTickerTickSpacing = 9 * tickerTickSpacing
+			//utils.LogDebug("Updating.. " + lastUpdateTime.Format(utils.DateTimeFormat) + " - " + fmt.Sprint(len(jobs)) + " jobs")
 			lastUpdateTime = lastUpdateTime.Add(tickerUpdateSpacing)
 			for _, j := range jobs {
-				utils.LogDebug("TICKING JOB ID : " + fmt.Sprint(j.Id))
+				//utils.LogDebug("TICKING JOB ID : " + fmt.Sprint(j.Id))
 				if time.Now().UTC().Sub(j.LastDoneTime) > j.Spacing {
 					c <- j
 				}
@@ -70,5 +73,5 @@ func AddJob(d time.Duration, memory interface{}, f func(*interface{})) {
 		Memory:       memory,
 		Do:           f,
 	}
-	utils.LogDebug("Job Added - " + fmt.Sprint(len(jobs)))
+	utils.LogInfo("Job added to background worker")
 }
