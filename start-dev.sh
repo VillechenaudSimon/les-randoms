dd60dbf2d5d7efa31f47a1f4094d7810f4c3d311
svillechenau <simon.villechenaud@etu.u-bordeaux.fr>
2022-07-25 11:54:40 +0200

    Edit start-dev

--- start-dev.sh
+++ start-dev.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-chrome.exe http://vemuni.ga/ https://github.com/Vemuni/les-randoms http://localhost:5000/ &
+chrome.exe http://vemuni.gq/ https://github.com/Vemuni/les-randoms http://localhost:5000/ &
 code . &
 
-# Use this git bash to git add/commit/push and to ./local-run.sh
+# Use this git bash to git add/commit/push 
 git-bash.exe &
 
-# Use this git bash to heroku login and git push heroku main and git logs --tail
+# Use this git bash to ./local-run.sh
 git-bash.exe &
